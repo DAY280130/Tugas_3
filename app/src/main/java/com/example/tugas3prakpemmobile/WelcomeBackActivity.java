a98bb22a6ca5fb1d5f9aa129e1a147bd16b03553
DAY280130 <dimas.280130@gmail.com>
2022-09-25 21:32:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas3prakpemmobile/WelcomeBackActivity.java
@@ -0,0 +1,15 @@
+package com.example.tugas3prakpemmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeBackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_back);
+//        getActionBar().hide();
+    }
+}
